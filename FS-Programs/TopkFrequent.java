ca95d3f4bdc73030d1bc7a5d9f25630b90a7c9ba
Surya Alakanti <alakantisurya13@gmail.com>
2025-03-10 20:08:13 +0530

    added solution

--- FS-Programs/TopkFrequent.java
+++ FS-Programs/TopkFrequent.java
@@ -13,7 +13,7 @@ Line-3: An integer k, representing the number of most frequent elements to retur
 Output Format:
 --------------
 Line-1: An array, comma-separated integers in descending order of frequency. 
-If two elements have the same frequency, the higher number should appear first.
+If two list have the same frequency, the higher number should appear first.
 
 
 Sample Input-1:
@@ -36,3 +36,33 @@ Sample Input-2:
 Sample Output-2:
 ----------------
 [1] */
+
+import java.util.*;
+class TopkFrequent {
+    static ArrayList<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>(hm.keySet());
+        list.sort((a, b) -> {
+            if (!hm.get(a).equals(hm.get(b))) {
+                return hm.get(b) - hm.get(a);
+            }
+            return b - a;
+        });
+        ArrayList<Integer> result = new ArrayList<>(list.subList(0, k));
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(Arrays.toString(topKFrequent(arr, k).toArray()));
+    }
+}
